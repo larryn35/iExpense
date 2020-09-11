16517c75ae0d6e0dc3e059e952d28f38839355cd
Larry N <larryn35@gmail.com>
2020-09-11 15:27:37 -0700

    add test expense item to list

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,9 +8,39 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+
 struct ContentView: View {
+    
+    @ObservedObject var expenses = Expenses()
+
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 4)
+                    self.expenses.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
     }
 }
 
